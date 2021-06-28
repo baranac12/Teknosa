5912f2f381970f3a74de56c993c04afdf719797c
baranac12 <61044071+baranac12@users.noreply.github.com>
2021-06-28 23:21:41 +0300

    Update Constants.kt

--- app/src/main/java/com/example/teknosa/Utils/Constants.kt
+++ app/src/main/java/com/example/teknosa/Utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.teknosa.Utils
 
 object Constants {
-    val BASE_URL="https://raw.githubusercontent.com/baranac12/h5190032barancananac_teknosa/main/"
+    val BASE_URL="https://raw.githubusercontent.com/baranac12/teknosaData/main/"
     var TIKLANAN_CATEGORY = "tiklananKategori"
     var TIKLANAN_URUN = "tiklananUrun"
 }
